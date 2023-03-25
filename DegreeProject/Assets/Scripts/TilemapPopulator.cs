dfeee4cb0e80285184c9960ae7b5fc459cd770b8
Jesse <essej.viitanen@gmail.com>
2023-03-25 03:23:49 +0100

    .

--- DegreeProject/Assets/Scripts/TilemapPopulator.cs
+++ DegreeProject/Assets/Scripts/TilemapPopulator.cs
@@ -16,17 +16,12 @@ public class TilemapPopulator : MonoBehaviour
 
     public TileGridStruct tileGrid;
 
-    [SerializeField]
-    List<KeyValuePair<Vector2Int, TileBase>> FetchedTiles;
-
     private void Start()
     {
         tileGrid = new();
         tileGrid.grid = TopDownGrid;
 
         tileGrid.tilemaps = tileGrid.grid.GetComponentsInChildren<Tilemap>();
-        Debug.Log($"Tilemaps: {tileGrid.tilemaps.Length}");
-
     }
 
     [ContextMenu("Run FetchMap")]
@@ -42,7 +37,6 @@ public class TilemapPopulator : MonoBehaviour
         scTransform.position = new Vector3(scTransform.position.x, scTransform.position.y, fetchedZposition);
 
         int searchWidth = 10;
-        Debug.Log(rotationZ);
 
         //search through and fill Fetched tiles with tiles form topdowngrid;
         for(int i = 0; i < tileGrid.tilemaps.Length; i++)
@@ -52,26 +46,14 @@ public class TilemapPopulator : MonoBehaviour
                 for (int x = fetchedXposition - searchWidth; x < fetchedXposition + searchWidth; x++)
                 {
                     sideScrollerMap.SetTile(new Vector3Int(x, Mathf.FloorToInt(tileGrid.tilemaps[i].transform.position.y - 1), 0), tileGrid.tilemaps[i].GetTile(new Vector3Int(x*rotationX, fetchedZposition, 0)));
-                    //FetchedTiles.Add(new KeyValuePair<new Vector2Int(x, 0), topDownMap.GetTile(new Vector3Int(x, 0, fetchedZposition)) > );
                 }
-                //for (int x = fetchedXposition + searchWidth; x > fetchedXposition - searchWidth; x--)
-                //{
-                //    sideScrollerMap.SetTile(new Vector3Int(x, Mathf.FloorToInt(tileGrid.tilemaps[i].transform.position.y - 1), 0), tileGrid.tilemaps[i].GetTile(new Vector3Int(x, fetchedZposition, 0)));
-                //    //FetchedTiles.Add(new KeyValuePair<new Vector2Int(x, 0), topDownMap.GetTile(new Vector3Int(x, 0, fetchedZposition)) > );
-                //}
             }
             else if(angleCheck == -1)
             {
                 for (int z = fetchedZposition - searchWidth; z < fetchedZposition + searchWidth; z++)
                 {
                     sideScrollerMap.SetTile(new Vector3Int(z, Mathf.FloorToInt(tileGrid.tilemaps[i].transform.position.y - 1), 0), tileGrid.tilemaps[i].GetTile(new Vector3Int(fetchedXposition, z*rotationZ, 0)));
-                    //FetchedTiles.Add(new KeyValuePair<new Vector2Int(x, 0), topDownMap.GetTile(new Vector3Int(x, 0, fetchedZposition)) > );
                 }
-                //for (int z = fetchedZposition + searchWidth; z > fetchedZposition - searchWidth; z--)
-                //{
-                //    sideScrollerMap.SetTile(new Vector3Int(z, Mathf.FloorToInt(tileGrid.tilemaps[i].transform.position.y - 1), 0), tileGrid.tilemaps[i].GetTile(new Vector3Int(fetchedXposition, z, 0)));
-                //    //FetchedTiles.Add(new KeyValuePair<new Vector2Int(x, 0), topDownMap.GetTile(new Vector3Int(x, 0, fetchedZposition)) > );
-                //}
             }
         }
 
